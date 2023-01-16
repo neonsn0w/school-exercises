1a95ce011d67ed605485f5036b7415076259fd1d
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-01-16 08:38:41 +0000

    indovinachi

---
+++ lab/casaosthegame.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int soluzione, utente, tentativi = 0;
+
+    srand(time(NULL));
+    
+    soluzione = rand() % 100 + 1;
+
+    cout << "Indovina il numero da 1 a 100" << endl;
+
+    while (soluzione != utente) {
+        cin >> utente;
+        tentativi++;
+        if (utente > soluzione) {
+            cout << "Il numero e' piu' piccolo" << endl;
+        } else if (utente < soluzione) {
+            cout << "Il numero e' piu' grande" << endl;
+        }
+    }
+
+    cout << "Hai indovinato il numero in " << tentativi << " tentativi" << endl;
+
+}
