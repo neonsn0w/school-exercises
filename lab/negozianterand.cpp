1aeb0974efcc9dde59d6a5da96abb765df509760
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-01-26 09:34:40 +0000

    modified negozianterand.cpp to work with functions

--- lab/negozianterand.cpp
+++ lab/negozianterand.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-int main() {
-    int prezzo, soldi = 0, resto;
-
+int genprezzo() {
+    int prezzo;
     srand(time(NULL));
-
     prezzo = rand() % 1000 + 1;
+    return prezzo;
+}
 
-    cout << "Il prezzo del prodotto e' " << prezzo << " euro." << endl;
-
+int genbill(int prezzo) {
+    int soldi = 0;
+    srand(time(NULL));
     while (prezzo>soldi) {
         soldi = rand() % 8 + 1;
         if (soldi == 1){
@@ -33,11 +34,20 @@ int main() {
             soldi = 1000;
         }
     }
+    return soldi;
+}
+
+int main() {
+    int prezzo, soldi = 0, resto;
+    srand(time(NULL));
+
+    prezzo = genprezzo();
+    cout << "Il prezzo del prodotto e' " << prezzo << " euro." << endl;
     
+    soldi = genbill(prezzo);
     resto = soldi - prezzo;
 
     cout << "Il cliente ha pagato " << soldi << " euro." << endl;
-
     cout << "Il resto e' " << resto << " euro." << endl;
 
     return 0;
