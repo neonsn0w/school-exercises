68b1b174ebd99317809c1d6203f4f5f84df56513
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-02-17 19:49:15 +0000

    fixed registrosort

--- classe/registrosort.cpp
+++ classe/registrosort.cpp
@@ -9,21 +9,20 @@ void inserimentoDati(int v1[], string v2[], int dim) {
     for (int i = 0; i < dim; i++) {
         int v;
         string n;
-        cout << "Inserire il voto dello studente " << i << " = ";
+        cout << "Inserire il voto del " << i+1 << " studente = ";
         cin >> v;
-        cout << "Inserire il nome dello studente " << i << " = ";
-        cin >> n;
-        cout << endl;
-
         v1[i] = v;
-        v2[i] = n;
+        cin.ignore();
+        cout << "Inserire il nome del " << i+1 << " studente = ";
+        getline(cin, v2[i]);
+        cout << endl;
     }
 }
 
-void ordinaDati(int v1[], string v2[], int dim) {       
+void ordinaDati(int v1[], string v2[], int dim) {
     string stemp;
     int temp;
-    for (int i; i < dim-1; i++) {
+    for (int i=0; i < dim-1; i++) {
         for (int j=i+1; j < dim; j++) {
             if (v1[i] > v1[j]) {
                 temp = v1[i];
@@ -38,17 +37,41 @@ void ordinaDati(int v1[], string v2[], int dim) {
     }
 }
 
-void stampaArray(int v1[], string v2[], int dim) {
-    for (int i = 0; i < dim; i++) {
-        cout << "Studente " << i << " = " << v1[i] << endl << "Voto = " << v2[i] << endl << endl;
+void ordinaNomi(string v2[], int dim) {
+    string stemp;
+    for (int i=0; i < dim-1; i++) {
+        for (int j=i+1; j < dim; j++) {
+            if (v2[i] > v2[j]) {
+                stemp = v2[i];
+                v2[i] = v2[j];
+                v2[j] = stemp;
+            }
+        }
     }
 }
+
+void stampaArray(int v1[], string v2[], int dim) {
+    for (int i = 0; i < dim; i++) {
+        cout << "Studente " << i << " = " << v2[i] << endl << "Voto = " << v1[i] << endl << endl;
+    }
+}
+
+void stampaNomi(string v2[], int dim) {
+    for (int i = 0; i < dim; i++) {
+        cout << "Studente " << i << " = " << v2[i] << endl;
+    }
+}
+
 
 int main() {
     int x;
 
     cout << "Inserire il numero di studenti: ";
     cin >> x;
+    while (x < 1) {
+        cout << endl << "Inserire un numero maggiore di 0: ";
+        cin >> x;
+    }
     cout << endl << endl;
     const int DIM = x;
     int voti[DIM];
@@ -59,6 +82,10 @@ int main() {
     ordinaDati(voti, nomi, DIM);
 
     stampaArray(voti, nomi, DIM);
+
+    ordinaNomi(nomi, DIM);
+
+    stampaNomi(nomi, DIM);
 
     return 0;
 }
