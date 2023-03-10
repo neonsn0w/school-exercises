e8e2c2e43868208d43f29a95a5a5c7a7d030fc82
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-03-10 19:36:28 +0000

    fixed string.cpp

--- classe/string.cpp
+++ classe/string.cpp
@@ -4,12 +4,50 @@
 
 using namespace std;
 
+// Fuznione per pulire la console (cross-platform)
+void clear() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Funzione per mettere in pausa l'esecuzione (cross-platform)
+void pause() {
+#ifdef _WIN32
+    system("pause");
+#else
+    system("read -p 'Premi Enter per continuare...' var");
+#endif
+}
+
+// Funzione per stampare il menu
+int menu(bool emptyString) {
+    int scelta;
+    cout << endl << "1. Inserisci stringa" << endl;
+    if (!emptyString) {
+        cout << "2. Visualizza la stringa" << endl;
+        cout << "3. Conta le vocali della stringa" << endl;
+        cout << "4. Aggiungi un'altra stringa nella stringa principale" << endl;
+        cout << "5. Estrai una sottostringa data la posizione di inizio e la lunghezza" << endl;
+    }
+    cout << endl << "0. Esci" << endl;
+
+    cout << endl << "Inserisci scelta: ";
+
+    cin >> scelta;
+    return scelta;
+}
+
+// Funzione per contare i caratteri di una stringa
 int contaChar(string s) {
     int dim = 0;
     while (s[dim++] != '\0');
     return dim;
 }
 
+// Funzione per contare le vocali di una stringa
 int contaVocali(string s, int dim) {
     int nvocali = 0;
     for (int i = 0; i < dim-1; i++) {
@@ -20,48 +58,108 @@ int contaVocali(string s, int dim) {
     return nvocali;
 }
 
-string inserisciStringa(string s1, string s2, int dim1, int dim2, int pos) {
-    string s3 = s1;
-    char tempchar = 'G';
+// Funzione per inserire una stringa in un'altra
+void inserisciStringa(string &s1, string s2, int pos) {
+    string s3 = "";
 
-    for(int i=dim1; i<dim2+dim1-1; i++) {
-        s3 = s3 + tempchar;
+    for (int i = 0; i < pos; i++) {
+        s3 += s1[i];
     }
 
-    for(int i=pos; i<pos+dim2-1; i++) {
-        s3[i+dim2-1] = s3[i];
+    s3 += s2;
+
+    for (int i = pos; i < contaChar(s1); i++) {
+        s3 += s1[i];
     }
 
-    for(int i=0; i<dim2-1; i++) {
-        s3[i+pos] = s2[i];
-    }
+    s1 = s3;
+}
 
-    return s3;
+// Funzione per estrarre una sottostringa data la posizione di inizio e la lunghezza
+string sottostringa (string s1, int pos, int lung) {
+    string s2 = "";
+    for (int i = pos; i < pos+lung; i++) {
+        s2 += s1[i];
+    }
+    return s2;
 }
 
 int main() {
     string s1, s2;
-    int pos;
+    int pos, scelta, dim1, dim2, nvocali, lung;
+    bool emptyString = true;
 
-    cout << "Inserisci una stringa: ";    
-    getline(cin, s1);
+     do {
+        clear();
+        scelta = menu(emptyString);
 
-    int dim1 = contaChar(s1);
-    cout << "La stringa inserita ha " << dim1+1 << " caratteri" << endl;
+        switch(scelta) {
+        case 0:
+            break;
 
-    int nvocali = contaVocali(s1, dim1);
+        case 1:
+            clear();
+            cout << "Inserisci una stringa: ";
+            cin.ignore();
+            getline(cin, s1);
+            emptyString = false;
+            break;
+        
+        case 2:
+            if (emptyString) {
+                break;
+            }
+            clear();
+            cout << "La stringa inserita è " << s1 << endl;
+            pause();
+            break;
 
-    cout << "La stringa inserita ha " << nvocali << " vocali" << endl;
+        case 3:
+            if (emptyString) {
+                break;
+            }
+            clear();
+            dim1 = contaChar(s1);
+            nvocali = contaVocali(s1, dim1);
+            cout << "La stringa inserita ha " << nvocali << " vocali" << endl;
+            pause();
+            break;
 
-    cout << "Inserisci un'altra stringa: ";
-    getline(cin, s2);
-    cout << "Digitare la posizione in cui inserire la seconda stringa: ";
-    cin >> pos;
+        case 4:
+            if (emptyString) {
+                break;
+            }
+            clear();
+            cout << "Inserisci un'altra stringa: ";
+            cin.ignore();
+            getline(cin, s2);
+            cout << "Digitare la posizione in cui inserire la seconda stringa: ";
+            cin >> pos;
 
-    int dim2 = contaChar(s2);
+            dim2 = contaChar(s2);
 
-    string s3 = inserisciStringa(s1, s2, dim1, dim2, pos);
-    cout << "Il risultato è " << s3;
+            inserisciStringa(s1, s2, pos);
+            cout << "Il risultato è " << s1 << endl;
+            pause();
+            break;
+
+        case 5:
+            if (emptyString) {
+                break;
+            }
+            clear();
+            cout << "Inserisci la posizione di inizio: ";
+            cin >> pos;
+            cout << "Inserisci la lunghezza: ";
+            cin >> lung;
+
+            s2 = sottostringa(s1, pos, lung);
+
+            cout << "La sottostringa è " << s2 << endl;
+            pause();
+            break;
+        }
+    } while (scelta != 0);
 
     return 0;
 }
