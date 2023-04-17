99d056f5a1965e04aa9a79a7d440c8d9bd88368e
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-04-17 06:39:33 +0000

    python ex 1 and 2

---
+++ python/es1.py
@@ -0,0 +1,4 @@
+# Scrivere un programma che dato n stampi a video il numero di ore in n settimane.
+n = int(input("Inserisci il nuemro di settimane: "))
+nn = n * 7 * 24
+print("Il numero di ore in", n, "settimane e'", nn)
---
+++ python/es2.py
@@ -0,0 +1,6 @@
+# Scrivere un programma che legga da tastiera s1 e s2 e ritorni
+# (stampi a video) True se s2 è sottostringa di s1, False altrimenti.
+# Non serve utilizzare strutture di controllo.
+s1 = input("Inserisci la prima stringa: ")
+s2 = input("Inserisci la seconda stringa: ")
+print(s2 in s1)
