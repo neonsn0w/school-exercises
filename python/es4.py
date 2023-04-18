b4d0178bb2494de88acc3aa85f1ce60675ecf138
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-04-18 08:50:18 +0000

    python 4

---
+++ python/es4.py
@@ -0,0 +1,5 @@
+stringa = input("Inserisci una stringa: ")
+marcatore = input("Inserisci il marcatore: ")
+sostitutore = input("Inserisci il sostitutore: ")
+
+print(stringa.replace(marcatore, sostitutore))
