021c193a10fe350b30efb3324898a587d362e62c
neonsn0w <85235959+neonsn0w@users.noreply.github.com>
2023-04-19 06:18:32 +0000

    python cicli es1

---
+++ python/cicli/es1.py
@@ -0,0 +1,9 @@
+n1 = input("Insesrisci il primo numero: ")
+n2 = input("Insesrisci il secondo numero: ")
+
+if n1 > n2:
+    print("Il primo numero e' maggiore del secondo")
+elif n2 < n1:
+    print("Il secondo numero e' maggiore del primo")
+else:
+    print("I due numeri sono uguali")
